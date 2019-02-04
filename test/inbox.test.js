8a679ff1fbc707f14e117eb900cfeb041ec67acd
ptmonline <ptmonlinework@gmail.com>
2019-02-04 10:13:02 +0100

    added first test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,4 +1,34 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+// const web3 = new Web3(ganache.provider());
+const web3 = new Web3(new Web3.providers.HttpProvider());
+
+
+
+class Car {
+    park(){
+        return 'stopped';
+    }
+
+    drive(){
+        return 'vroom';
+    }
+}
+
+let car; 
+
+beforeEach(()=>{
+    car = new Car();
+});
+
+describe('Car', ()=>{
+
+    it('Should return stopped',()=>{
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('should return vroom',()=>{
+        assert.equal(car.drive(), 'vroom');
+    })
+});
