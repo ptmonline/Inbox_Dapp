c3ba32435ca01bbafff84950b4082bb16c5cc110
ptmonline <ptmonlinework@gmail.com>
2019-02-05 09:11:15 +0100

    fixed web3 version

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,34 +1,48 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-// const web3 = new Web3(ganache.provider());
-const web3 = new Web3(new Web3.providers.HttpProvider());
+const provider = ganache.provider();
+const web3 = new Web3(ganache.provider());
 
-
-
-class Car {
-    park(){
-        return 'stopped';
-    }
-
-    drive(){
-        return 'vroom';
-    }
-}
-
-let car; 
-
-beforeEach(()=>{
-    car = new Car();
-});
-
-describe('Car', ()=>{
-
-    it('Should return stopped',()=>{
-        assert.equal(car.park(), 'stopped');
+let accounts;
+beforeEach(() => {
+    // Get a list of all accounts
+    web3.eth.personal.getAccounts().then(a => {
+        accounts = a;
+            console.log(a);
     });
-
-    it('should return vroom',()=>{
-        assert.equal(car.drive(), 'vroom');
-    })
 });
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts);
+     });
+})
+
+
+// class Car {
+//     park(){
+//         return 'stopped';
+//     }
+
+//     drive(){
+//         return 'vroom';
+//     }
+// }
+
+// let car; 
+
+// beforeEach(()=>{
+//     car = new Car();
+// });
+
+// describe('Car', ()=>{
+
+//     it('Should return stopped',()=>{
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('should return vroom',()=>{
+//         assert.equal(car.drive(), 'vroom');
+//     })
+// });
