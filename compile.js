cc2867790c460e0baa7515f60b7451344d2bffea
ptmonline <ptmonlinework@gmail.com>
2019-02-05 15:28:50 +0100

    created deploy.js file

--- compile.js
+++ compile.js
@@ -6,4 +6,6 @@ const inboxPath = path.resolve(__dirname + '/contracts', 'inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 // console.log(solc.compile(source, 1));
+// INFURA ENDPOINT = https://rinkeby.infura.io/v3/4404778bc8d64811b4f531e508d9817c
+
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
